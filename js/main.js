a59220fd2d137a7bf6452dab1a1cef9aa721fb29
3axap <bagrii.zakhar@gmail.com>
2022-02-18 13:59:17 +0200

    add js code & add new classes css code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
 
 item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
-
-function dragstart(){
-    console.log('drag start')
+for(const placeholder of placeholders){
+    placeholder.addEventListener('dragover',dragover)
+    placeholder.addEventListener('dragenter',dragenter)
+    placeholder.addEventListener('dragleave',dragleave)
+    placeholder.addEventListener('drop',drop)
 }
-function dragend(){
-    console.log('drag end')
+
+function dragstart(e){
+    e.target.classList.add('hold')
+    setTimeout(()=> e.target.classList.add('hide') , 0)
+}
+function dragend(e){
+    e.target.classList.remove('hold' , 'hide')
+}
+
+function dragover (e){
+    console.log('drag over')
+}
+function dragenter (e){
+    console.log('drag enter')
+}
+function dragleave (e){
+    console.log('drag leave')
+}
+function drop (e){
+    console.log('drop')
 }
